e152d2dd8d16c56d00a78448be0c6cdf0914ca2b
Alexey <aleksey.nen@yandex.ru>
2021-11-17 11:03:06 +0300

    feat: добавить новый проект Autotest

---
+++ src/test/java/ru/mail/nenado/MyFirstTest.java
@@ -0,0 +1,31 @@
+package ru.mail.nenado;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class MyFirstTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void myFirstTest() {
+        driver.get("http://www.google.com/");
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
